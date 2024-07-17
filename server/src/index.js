b9d9a8a1ddb0b5ef137da76dae56227657f0e3a5
Abhaya N K <abhayakhatri48@gmail.com>
2024-07-17 10:47:48 +0545

    projet

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+const userRoute = require('./routes/users')
+const connect = require('./db/connection')
+app.use(userRoute)
+connect()
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
